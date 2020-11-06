d17f1458602019883a5dd1927f7c23e5c82bbbb7
CGC <70499508+cgconverse@users.noreply.github.com>
2020-11-05 19:02:45 -0800

    created quiz timer

--- codingscript.js
+++ codingscript.js
@@ -5,3 +5,23 @@ startButton.addEventListener("click", ()=>{
     console.log("start");
 })
 
+//----Quiz Timer----//
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 20;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " Seconds left!";
+
+ if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }   
+
+  }, 1000);
+}
+
+setTime();
