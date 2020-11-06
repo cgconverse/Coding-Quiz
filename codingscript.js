ea0cae4e9a936e4d4d93b8747f1b90a54dfb5504
CGC <70499508+cgconverse@users.noreply.github.com>
2020-11-05 23:41:08 -0800

    changed color scheme

--- codingscript.js
+++ codingscript.js
@@ -23,7 +23,49 @@ var secondsLeft = 20;
       }
       
       setTime();
-})
+
+
+//----Questions with multiple choice----//
+var myQuestions = [
+    {
+      question: "Where is Forrest from?",
+      answers: {
+        a: "Georgia",
+        b: "Alabama",
+        c: "Tennessee"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What is the name of Forrest's one true love?",
+      answers: {
+        a: "Jenny",
+        b: "Penny",
+        c: "Minnie"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What does Forrest's mama always say?",
+      answers: {
+        a: "Life is like a dream",
+        b: "Life is funny",
+        c: "Life goes on",
+        d: "Life is like a box of chocolates"
+      },
+      correctAnswer: "d"
+    }
+  ];
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
 
 
 
+
+
+
+  quizContainer.innerHTML = output.join('');
