94b3a742555a29de38b167be19e7ffb0bf49ca2d
CGC <70499508+cgconverse@users.noreply.github.com>
2020-11-07 15:21:12 -0800

    created html elements with css styling and start button

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Coding-Quiz
+## Coding-Quiz
+
+## Description
+
+The user will activate a 30 second timer once they press the 'start' button and the first question pops up.
+
+The user will then race against the timer to correctly answer a series of multiple choice questions. If the correct answer is selected, it will be highlighted in green. If the wrong answer is chosen, it will be highlighted in red.
+
+Each time the user chooses an incorrect answer, 5 seconds will be deducted from the timer.
+
+Once the user selects thier answer, another question will be displayed.
+
+When the timer runs out or when the user completes the quiz, the game is over. A score board will then be displayed where the user can save their initials and score to the score board.
+
+## Credits
+
+I used Bootstrap to 
+
+
--- codingscript.js
+++ codingscript.js
@@ -1,71 +1,130 @@
 //----Start button----//
-var startButton = document.getElementById("start")
+// var startButton = document.getElementById("start")
 
-startButton.addEventListener("click", ()=>{
+// startButton.addEventListener("click", ()=>{
     
-    //----Quiz Timer----//
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
+//     //----Quiz Timer----//
+// var timeEl = document.querySelector(".time");
+// var mainEl = document.getElementById("main");
 
-var secondsLeft = 20;
+// var secondsLeft = 20;
 
-    function setTime() {
-        var timerInterval = setInterval(function() {
-          secondsLeft--;
-          timeEl.textContent = secondsLeft + " Seconds left!";
+//     function setTime() {
+//         var timerInterval = setInterval(function() {
+//           secondsLeft--;
+//           timeEl.textContent = secondsLeft + " Seconds left!";
       
-       if(secondsLeft === 0) {
-            clearInterval(timerInterval);
+//        if(secondsLeft === 0) {
+//             clearInterval(timerInterval);
             
-          }   
+//           }   
       
-        }, 1000);
-      }
+//         }, 1000);
+//       }
       
-      setTime();
+//       setTime();
 
 
-//----Questions with multiple choice----//
-var myQuestions = [
-    {
-      question: "Where is Forrest from?",
-      answers: {
-        a: "Georgia",
-        b: "Alabama",
-        c: "Tennessee"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "What is the name of Forrest's one true love?",
-      answers: {
-        a: "Jenny",
-        b: "Penny",
-        c: "Minnie"
-      },
-      correctAnswer: "a"
-    },
-    {
-      question: "What does Forrest's mama always say?",
-      answers: {
-        a: "Life is like a dream",
-        b: "Life is funny",
-        c: "Life goes on",
-        d: "Life is like a box of chocolates"
-      },
-      correctAnswer: "d"
+// //----Questions with multiple choice----//
+// var myQuestions = [
+//     {
+//       question: "Where is Forrest from?",
+//       answers: {
+//         a: "Georgia",
+//         b: "Alabama",
+//         c: "Tennessee"
+//       },
+//       correctAnswer: "b"
+//     },
+//     {
+//       question: "What is the name of Forrest's one true love?",
+//       answers: {
+//         a: "Jenny",
+//         b: "Penny",
+//         c: "Minnie"
+//       },
+//       correctAnswer: "a"
+//     },
+//     {
+//       question: "What does Forrest's mama always say?",
+//       answers: {
+//         a: "Life is like a dream",
+//         b: "Life is funny",
+//         c: "Life goes on",
+//         d: "Life is like a box of chocolates"
+//       },
+//       correctAnswer: "d"
+//     }
+//   ];
+
+// document.getElementById("myBtn").addEventListener("click", displayDate);
+
+// function displayDate() {
+//   document.getElementById("demo").innerHTML = Date();
+// }
+
+//   quizContainer.innerHTML = output.join('');
+
+
+
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+//Starts the quiz
+function startQuiz() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+//Sets next question when 'Next' button is clicked
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
     }
-  ];
-
-document.getElementById("myBtn").addEventListener("click", displayDate);
-
-function displayDate() {
-  document.getElementById("demo").innerHTML = Date();
 }
 
 
+function selectAnswer(e) {
 
+}
 
-
-
-  quizContainer.innerHTML = output.join('');
+var questions = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+    }
+]
